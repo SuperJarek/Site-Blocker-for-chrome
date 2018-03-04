cc2c0825839994f9c21611aaa7ae082e5617c474
superJarek <jaroslaw.szymukowicz@gmail.com>
2018-03-04 18:01:23 +0100

    facebookBlock function takes action only if extension is enabled

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-chrome.runtime.onStartup.addListener(function (){
+chrome.runtime.onStartup.addListener(function initialization(){
 	var isEnabled = false;
 	chrome.storage.sync.set({'isEnabled': isEnabled}, function() {
 		console.log('Extension is disabled.');
 	});
 });
 
-function updateIcon(){
+chrome.browserAction.onClicked.addListener(function updateIcon(){
 	chrome.storage.sync.get('isEnabled', function(data){
 		var isEnabled = data.isEnabled;
 		var icon;
@@ -20,26 +20,28 @@ function updateIcon(){
 			console.log('Extension has been disabled/enabled.');
 		});	
 	});
-};
-
-chrome.browserAction.onClicked.addListener(updateIcon);
+});
 
 
 chrome.tabs.onUpdated.addListener(function closeFacebook(tabId , info , tab) {
-		console.log(tab.url);
-		if(tab.url.includes("://www.facebook")){
-			chrome.tabs.discard(tabId);
-			
-			/* Alternative way of dealing with tab no. 1
-			chrome.tabs.executeScript(tabId, {
-				code: 'document.body.innerHTML = "No facebook for you!"'
-			});
-			*/
-			
-			/* Alternative way no. 2
-			chrome.tabs.remove(tabId);
-			*/
-		}
+		chrome.storage.sync.get('isEnabled', function(data){
+			if(data.isEnabled){
+				console.log(tab.url);
+				if(tab.url.includes("://www.facebook")){
+					chrome.tabs.discard(tabId);
+					
+					/* Alternative way of dealing with tab no. 1
+					chrome.tabs.executeScript(tabId, {
+						code: 'document.body.innerHTML = "No facebook for you!"'
+					});
+					*/
+					
+					/* Alternative way no. 2
+					chrome.tabs.remove(tabId);
+					*/
+				}
+			}
+		});
 });
 
 
