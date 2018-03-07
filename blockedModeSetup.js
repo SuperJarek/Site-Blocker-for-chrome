3f8d89961476a97b4942a4c805a6fb814bfc02af
JanBan <baniecki.jan@gmail.com>
2018-03-07 18:38:01 +0100

    added blocking for specific time

--- blockedModeSetup.js
+++ blockedModeSetup.js
@@ -1,3 +1,5 @@
+const ENTER_KEY_CODE = 13;
+
 document.addEventListener('DOMContentLoaded', function() {
 	let textField = document.getElementById('duration');
 	textField.focus();
@@ -5,32 +7,34 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	document.getElementById('duration').addEventListener('keyup', function(event) {
 		event.preventDefault();
-		if (event.keyCode === 13) {
-			document.getElementById("start").click();
+		if (event.keyCode == ENTER_KEY_CODE) {
+			setTimer();
 		}
 	});
-	
     let startButton = document.getElementById('start');
     startButton.addEventListener('click', function() {
-		console.log("klikniete");
-		let currentDateMilliseconds = Date.now();
-		let durationMilliseconds = 
-			document.getElementById('duration').value * 60 * 1000;
-		let blockUntil = currentDateMilliseconds + durationMilliseconds;
-		let timerData = { isTimerEnabled: true, blockUntilMilliseconds: blockUntil};
-		chrome.storage.sync.set({'timerData': timerData}, function() {
-			console.log('Time set to ' + blockUntil);
-			timer();
-			return;
-		});
-		chrome.storage.sync.set({'isEnabled': true}, function() {
-		chrome.browserAction.setIcon({path: 'on.png'});
-			console.log('Set to enabled');
-		});
+		setTimer();
     });
 	
 	timer();
 });
+
+function setTimer(){
+	let currentDateMilliseconds = Date.now();
+	let durationMilliseconds = 
+		document.getElementById('duration').value * 60 * 1000;
+	let blockUntil = currentDateMilliseconds + durationMilliseconds;
+	let timerData = { isTimerEnabled: true, blockUntilMilliseconds: blockUntil};
+	chrome.storage.sync.set({'timerData': timerData}, function() {
+		console.log('Time set to ' + blockUntil);
+		timer();
+		return;
+	});
+	chrome.storage.sync.set({'isEnabled': true}, function() {
+	chrome.browserAction.setIcon({path: 'on.png'});
+		console.log('Set to enabled');
+	});
+}
 
 function timer(){
 	chrome.storage.sync.get('timerData', function (data) {
