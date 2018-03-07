fb9a528b6adf6d41e8a3b95dcb2a09dbd01107d3
superJarek <jaroslaw.szymukowicz@gmail.com>
2018-03-07 12:29:35 +0100

    table with buttons created, buttons recognizes which link they are responsible for

--- filterList.js
+++ filterList.js
@@ -1,5 +1,23 @@
-chrome.storage.sync.get('blockedSites', function (data){
-	data.blockedSites.forEach(function(site){
-		document.write(site + '<br />');
+document.addEventListener('DOMContentLoaded', function() {
+	chrome.storage.sync.get('blockedSites', function (data){
+		let div = document.getElementById('filterList');
+		let table = "<table>";
+		let counter = 0;
+		data.blockedSites.forEach(function(site){
+			
+			table += "<tr><td id =\"" + counter; 
+			table += "site\">";
+			table += site; 
+			table += "</td><td><button id =\""+ counter++ +"button\">&times;</button ></td></tr>";
+		});
+		table += "</table>";
+		div.innerHTML = table;
+		
+		let bns = document.getElementsByTagName("button");
+		for (i = 0; i < bns.length; i++) {
+			bns[i].addEventListener("click", function() {
+				alert(document.getElementById(this.id[0] + "site").innerHTML);
+			});
+		}	
 	});
 });
